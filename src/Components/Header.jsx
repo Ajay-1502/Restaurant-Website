dec73bd618a135f20a4efd0396fe6e3ff160c2ed
Ajay-1502 <ahdesai40@gmail.com>
2025-06-22 01:54:48 +0530

    Logic to calculate total quantity

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,17 @@
+import { useContext } from 'react';
 import meals from '../assets/meals.jpeg';
 import './Header.css';
 import CartIcon from './cartIcon';
 import RestaurantSummary from './RestaurantSummary';
+import CartContext from './store/cart-context';
 
 const Header = (props) => {
+  const CartCtx = useContext(CartContext);
+
+  const totalQuantity = CartCtx.items.reduce((sum, item) => {
+    return sum + item.quantity;
+  }, 0);
+
   return (
     <>
       <header className="header">
@@ -11,7 +19,7 @@ const Header = (props) => {
         <button className="cart-button" onClick={props.displayCart}>
           <CartIcon />
           <span>Your Cart - </span>
-          <span className="badge">0</span>
+          <span className="badge">{totalQuantity}</span>
         </button>
       </header>
       <div className="main-image">
